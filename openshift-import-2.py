612f48f791a033e421242c07d0e6cf6126f5fc98
Martin Bayr <ex.bayr@muenchen.de>
2023-07-05 16:50:24 +0200

    added import scripts

---
+++ openshift-import-2.py
@@ -0,0 +1,17 @@
+
+## Import the created capfiles to openshift
+## For that, be sure to be logged into openshift and right project, check with "oc projects"
+
+import os
+
+outfile_1 = 'openshift/capfile-1-imagestreams.yaml'
+outfile_2 = 'openshift/capfile-2-others.yaml'
+outfile_3 = 'openshift/capfile-3-ckan.yaml'
+outfile_4 = 'openshift/capfile-4-nginx.yaml'
+
+print(outfile_1)
+
+#os.system('oc create -f ' + outfile_1)
+#os.system('oc create -f ' + outfile_2)
+os.system('oc create -f ' + outfile_3)
+os.system('oc create -f ' + outfile_4)
