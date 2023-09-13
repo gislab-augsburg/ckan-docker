a1a1c1dc96e8ee1ee909f59be81e71979cf37634
Martin Bayr <ex.bayr@muenchen.de>
2023-09-13 15:38:12 +0200

    changing default package type

--- ckan/setup/ext_config.sh
+++ ckan/setup/ext_config.sh
@@ -1,3 +1,4 @@
-echo configuring ckan extansions
+echo Configuring ckan extensions
 # ckanext-scheming config
 ckan config-tool ${APP_DIR}/ckan.ini scheming.dataset_schemas=ckanext.scheming:camel_photos.yaml
+ckan config-tool ${APP_DIR}/ckan.ini ckan.default.package_type=camel_photos
