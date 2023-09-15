e436059106782daef92d012891afe3d5562801b0
Martin Bayr <ex.bayr@muenchen.de>
2023-09-15 16:45:13 +0200

    roll back camel-photo as dafault package

--- ckan/setup/ext_config.sh
+++ ckan/setup/ext_config.sh
@@ -1,4 +1,4 @@
 echo Configuring ckan extensions
 # ckanext-scheming config
 ckan config-tool ${APP_DIR}/ckan.ini scheming.dataset_schemas='ckanext.scheming:camel_photo.yaml ckanext.scheming:ckan_formpages.yaml'
-ckan config-tool ${APP_DIR}/ckan.ini ckan.default.package_type=camel-photo
+#ckan config-tool ${APP_DIR}/ckan.ini ckan.default.package_type=camel-photo
